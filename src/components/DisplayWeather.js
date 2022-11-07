f3d057e2ac2d9ed7ce951e663df1660bee1aec2b
Igor Boyko <iboyko@gmail.com>
2022-11-07 14:08:44 -0800

    removed comments

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -3,7 +3,6 @@ import "./displayweather.css";
 
 function DisplayWeather(props) {
     const {data} = props;
-    console.log(data)
     const iconurl = `http://openweathermap.org/img/wn/${data.cod !== 404 ? data.weather[0].icon : null}.png`;
     
     return (
